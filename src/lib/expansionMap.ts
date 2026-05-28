64be37baca03656b13c19c7b779e4aaf35d74937
wjbetech <wjbetech@gmail.com>
2026-05-28 19:54:36 +0900

    chore/architecture: deleted old stale code

--- src/lib/expansionMap.ts
+++
@@ -1,5 +0,0 @@
-import { expansionsData, type Expansion, type ExpansionConfig } from "../data/expansionData";
-
-export const expansionMap = Object.fromEntries(
-  expansionsData.map((exp) => [exp.id, exp]),
-) as Record<Expansion, ExpansionConfig>;
